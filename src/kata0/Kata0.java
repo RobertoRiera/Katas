c8ef13f583132169df2975fb11824eb68aafefe0
cantim <cantim@cantim-Aspire-5739G>
2013-10-09 15:59:36 +0100

    Kata 0 terminada

--- src/kata0/Kata0.java
+++ src/kata0/Kata0.java
@@ -4,11 +4,10 @@ import java.util.Date;
 
 public class Kata0 {
 
- 
     public static void main(String[] args) {
         // TODO code application logic here
         Date birthDate = new Date(92, 5, 1, 0, 0, 0);
-        Student Roberto = new Student ("Roberto", birthDate);
-        System.out.print("Tiene "+Roberto.getAge()+" años");
+        Student Roberto = new Student("Roberto", birthDate);
+        System.out.print(Roberto.getName() + " Tiene " + Roberto.getAge() + " años");
     }
 }
