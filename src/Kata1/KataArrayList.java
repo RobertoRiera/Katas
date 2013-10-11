049822f7b1e026831ca26554c03b233ad628deb0
usuario <usuario@10.22.146.194>
2013-10-11 10:08:40 +0100

    Commit Katas

--- src/Kata1/KataArrayList.java
+++ src/Kata1/KataArrayList.java
@@ -7,15 +7,17 @@ public class KataArrayList {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            list.add((int)(Math.random() * 50));
+            list.add((int) (Math.random() * 50));
         }
         for (Integer value : list) {
             System.out.println(value);
         }
         ArrayList<Integer> elderPeople = new ArrayList<>();
-        
+
         for (Integer value : list) {
-            if(value <= 70) continue;
+            if (value <= 70) {
+                continue;
+            }
             elderPeople.add(value);
         }
         list.removeAll(elderPeople);
